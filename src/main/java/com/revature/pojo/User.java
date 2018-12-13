a8e6fcb6601f5e80f978244cab4d8a5a453353f6
Hugo Fernandez <hugo.fernandez@telecomnancy.net>
2018-12-12 19:00:07 -0500

    User pojo with fields

--- src/main/java/com/revature/pojo/User.java
+++ src/main/java/com/revature/pojo/User.java
@@ -1,5 +1,52 @@
 package com.revature.pojo;
 
 public class User {
+	int userId;
+	String username;
+	String password;
+	Boolean isAdmin;
+	
+	
+	public User() {
+		super();
+	}
+	
+	
+	public User(int userId, String username, String password, Boolean isAdmin) {
+		super();
+		this.userId = userId;
+		this.username = username;
+		this.password = password;
+		this.isAdmin = isAdmin;
+	}
+	public int getUserId() {
+		return userId;
+	}
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public Boolean getIsAdmin() {
+		return isAdmin;
+	}
+	public void setIsAdmin(Boolean isAdmin) {
+		this.isAdmin = isAdmin;
+	}
+	
+	
+	
+	
+	
 
 }
