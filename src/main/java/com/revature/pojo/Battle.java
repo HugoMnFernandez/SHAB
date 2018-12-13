eb7e8c4c12e5237ae97bd5337e64f4efc487d282
Hugo Fernandez <hugo.fernandez@telecomnancy.net>
2018-12-12 19:18:03 -0500

    adding lists of character in battle to represent the teams and creating the comment pojo

--- src/main/java/com/revature/pojo/Battle.java
+++ src/main/java/com/revature/pojo/Battle.java
@@ -1,6 +1,7 @@
 package com.revature.pojo;
 
 import java.util.Date;
+import java.util.List;
 
 public class Battle {
 	private int battleId;
@@ -10,13 +11,15 @@ public class Battle {
 	private int Result;
 	private int team1Votes;
 	private int team2Votes;
+	private List<Character> team1;
+	private List<Character> team2;
 
 	public Battle() {
 		super();
 	}
 
 	public Battle(int battleId, String location, Date startDate, Date endDate, int result, int team1Votes,
-			int team2Votes) {
+			int team2Votes, List<Character> team1, List<Character> team2) {
 		super();
 		this.battleId = battleId;
 		this.location = location;
@@ -25,6 +28,8 @@ public class Battle {
 		Result = result;
 		this.team1Votes = team1Votes;
 		this.team2Votes = team2Votes;
+		this.team1 = team1;
+		this.team2 = team2;
 	}
 
 	public int getBattleId() {
@@ -81,6 +86,22 @@ public class Battle {
 
 	public void setTeam2Votes(int team2Votes) {
 		this.team2Votes = team2Votes;
+	}
+
+	public List<Character> getTeam1() {
+		return team1;
+	}
+
+	public void setTeam1(List<Character> team1) {
+		this.team1 = team1;
+	}
+
+	public List<Character> getTeam2() {
+		return team2;
+	}
+
+	public void setTeam2(List<Character> team2) {
+		this.team2 = team2;
 	}
 
 }
--- src/main/java/com/revature/pojo/Comment.java
+++ src/main/java/com/revature/pojo/Comment.java
@@ -2,4 +2,63 @@ package com.revature.pojo;
 
 public class Comment {
 
+	private int commentId;
+	private User user;
+	private Battle battle;
+	private int team;
+	private int votes;
+
+	public Comment(int commentId, User user, Battle battle, int team, int votes) {
+		super();
+		this.commentId = commentId;
+		this.user = user;
+		this.battle = battle;
+		this.team = team;
+		this.votes = votes;
+	}
+
+	public Comment() {
+		super();
+	}
+
+	public int getCommentId() {
+		return commentId;
+	}
+
+	public void setCommentId(int commentId) {
+		this.commentId = commentId;
+	}
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+
+	public Battle getBattle() {
+		return battle;
+	}
+
+	public void setBattle(Battle battle) {
+		this.battle = battle;
+	}
+
+	public int getTeam() {
+		return team;
+	}
+
+	public void setTeam(int team) {
+		this.team = team;
+	}
+
+	public int getVotes() {
+		return votes;
+	}
+
+	public void setVotes(int votes) {
+		this.votes = votes;
+	}
+
 }
