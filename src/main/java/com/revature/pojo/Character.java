cb3697a3402440dd1afd9149c8d9cf9d3edce97a
Hugo Fernandez <hugo.fernandez@telecomnancy.net>
2018-12-12 19:06:36 -0500

    format of User pojo and adding attributes and getters, setters and constructor to Character

--- src/main/java/com/revature/pojo/Character.java
+++ src/main/java/com/revature/pojo/Character.java
@@ -1,5 +1,63 @@
 package com.revature.pojo;
 
 public class Character {
+	int characterId;
+	int rank;
+	int win;
+	int losses;
+	double score;
+
+	public Character() {
+		super();
+	}
+
+	public Character(int characterId, int rank, int win, int losses, double score) {
+		super();
+		this.characterId = characterId;
+		this.rank = rank;
+		this.win = win;
+		this.losses = losses;
+		this.score = score;
+	}
+
+	public int getCharacterId() {
+		return characterId;
+	}
+
+	public void setCharacterId(int characterId) {
+		this.characterId = characterId;
+	}
+
+	public int getRank() {
+		return rank;
+	}
+
+	public void setRank(int rank) {
+		this.rank = rank;
+	}
+
+	public int getWin() {
+		return win;
+	}
+
+	public void setWin(int win) {
+		this.win = win;
+	}
+
+	public int getLosses() {
+		return losses;
+	}
+
+	public void setLosses(int losses) {
+		this.losses = losses;
+	}
+
+	public double getScore() {
+		return score;
+	}
+
+	public void setScore(double score) {
+		this.score = score;
+	}
 
 }
--- src/main/java/com/revature/pojo/User.java
+++ src/main/java/com/revature/pojo/User.java
@@ -5,13 +5,11 @@ public class User {
 	String username;
 	String password;
 	Boolean isAdmin;
-	
-	
+
 	public User() {
 		super();
 	}
-	
-	
+
 	public User(int userId, String username, String password, Boolean isAdmin) {
 		super();
 		this.userId = userId;
@@ -19,34 +17,37 @@ public class User {
 		this.password = password;
 		this.isAdmin = isAdmin;
 	}
+
 	public int getUserId() {
 		return userId;
 	}
+
 	public void setUserId(int userId) {
 		this.userId = userId;
 	}
+
 	public String getUsername() {
 		return username;
 	}
+
 	public void setUsername(String username) {
 		this.username = username;
 	}
+
 	public String getPassword() {
 		return password;
 	}
+
 	public void setPassword(String password) {
 		this.password = password;
 	}
+
 	public Boolean getIsAdmin() {
 		return isAdmin;
 	}
+
 	public void setIsAdmin(Boolean isAdmin) {
 		this.isAdmin = isAdmin;
 	}
-	
-	
-	
-	
-	
 
 }
