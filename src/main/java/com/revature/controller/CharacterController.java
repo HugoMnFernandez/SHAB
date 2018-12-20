5d6698dd9d177f208eac055b0d21112afea1c79f
Hugo Fernandez <hugo.fernandez@telecomnancy.net>
2018-12-20 10:45:25 -0500

    adding a character controller

---
+++ src/main/java/com/revature/controller/CharacterController.java
@@ -0,0 +1,35 @@
+package com.revature.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.revature.pojo.Character;
+import com.revature.service.CharacterService;
+
+@RestController
+@RequestMapping("/character")
+@CrossOrigin(origins="*")
+public class CharacterController {
+	
+	@Autowired
+	CharacterService cServ;
+	
+	
+	@PostMapping(consumes = "application/json")
+	public void makeCharacter(@RequestBody Character character) {
+		cServ.createCharacter(character);
+	}
+	
+	@GetMapping("/{id}")
+	public Character getCharacterById(@PathVariable int id) {
+		return cServ.getCharacterById(id);
+	}
+	
+
+}
